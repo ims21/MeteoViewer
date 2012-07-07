7d5511e684d194e7a04f41e00f1b4d4bac62c244
ims <ims21@users.sourceforge.net>
2012-07-07 12:13:47 +0200

    repaired indentation setup_translate.py

--- setup_translate.py
+++ setup_translate.py
@@ -5,32 +5,33 @@ from distutils.command.build import build as _build
 import os
 
 class build_trans(cmd.Command):
-description = 'Compile .po files into .mo files'
-def initialize_options(self):
-pass
+	description = 'Compile .po files into .mo files'
+	def initialize_options(self):
+		pass
 
-def finalize_options(self):
-pass
+	def finalize_options(self):
+		pass
 
-def run(self):
-s = os.path.join('plugin', 'locale')
-for lang in os.listdir(s):
-lc = os.path.join(s, lang, 'LC_MESSAGES')
-if os.path.isdir(lc):
-for f in os.listdir(lc):
-if f.endswith('.po'):
-src = os.path.join(lc, f)
-dest = os.path.join(lc, f[:-2] + 'mo')
-print "Language compile %s -> %s" % (src, dest)
-if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
-raise Exception, "Failed to compile: " + src
+	def run(self):
+		s = os.path.join('plugin', 'locale')
+		for lang in os.listdir(s):
+			lc = os.path.join(s, lang, 'LC_MESSAGES')
+			if os.path.isdir(lc):
+				for f in os.listdir(lc):
+					if f.endswith('.po'):
+						src = os.path.join(lc, f)
+						dest = os.path.join(lc, f[:-2] + 'mo')
+						print "Language compile %s -> %s" % (src, dest)
+						if os.system("msgfmt '%s' -o '%s'" % (src, dest)) != 0:
+							raise Exception, "Failed to compile: " + src
 
 class build(_build):
-sub_commands = _build.sub_commands + [('build_trans', None)]
-def run(self):
-_build.run(self)
+	sub_commands = _build.sub_commands + [('build_trans', None)]
+	def run(self):
+		_build.run(self)
 
 cmdclass = {
-'build': build,
-'build_trans': build_trans,
+	'build': build,
+	'build_trans': build_trans,
 }
+
